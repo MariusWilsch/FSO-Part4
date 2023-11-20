d8cb4616cd6c906fa331d4f2532deb92a334c076
Marius Santiago Wilsch <84270258+VerdantStartup@users.noreply.github.com>
2023-11-20 20:52:33 +0100

    Stopped at part5/a/Saving the token to the browser's local storage

--- blogList-backend/index.js
+++
@@ -1,5 +0,0 @@
-const app = require("./app.js");
-const config = require("./utils/config.js");
-const { info } = require("./utils/logger.js");
-
-app.listen(config.PORT, () => info(`Server running on port ${config.PORT}`));
